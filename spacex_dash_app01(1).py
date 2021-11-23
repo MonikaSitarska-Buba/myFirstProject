3047cbbab14c7e20954baa72a74447eb6cea74db
MonikaSitarska-Buba <74401907+MonikaSitarska-Buba@users.noreply.github.com>
2021-11-23 11:54:23 +0100

    Update spacex_dash_app01(1).py

--- spacex_dash_app01(1).py
+++ spacex_dash_app01(1).py
@@ -61,10 +61,11 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
 
 # TASK 2:
 # Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
-@app.callback(Output(component_id='success-pie-chart',component_property='figure'),
-Input(component_id='site-dropdown',component_property='value'))
+@app.callback(
+  Output(component_id='success-pie-chart',component_property='figure'),
+  Input(component_id='site-dropdown',component_property='value'))
 
-def get_pie_chart(site):
+def pie_chart(site):
     filtered_df=spacex_df
     if site=='ALL':
         fig=px.pie(spacex_df,values='class',
@@ -78,7 +79,8 @@ def get_pie_chart(site):
         return fig
 # TASK 4:
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
-@app.callback(Output(component_id='success-payload-scatter-chart',component_property='figure'),
+@app.callback(
+                Output(component_id='success-payload-scatter-chart',component_property='figure'),
                 [Input(component_id='site-dropdown',component_property='value'),
                 Input(component_id='payload-slider',component_property='value')])
 def scatter(site,payload):
